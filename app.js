0e98492d4cb41a1def0bbf5e40b1cba93a2cd7b8
Stanley <stanleypl75@gmail.com>
2019-05-28 15:51:51 -0400

    Data-date  align with the corresponding value on the x-axis.

--- app.js
+++ app.js
@@ -1,85 +1,102 @@
-req=new XMLHttpRequest();
-req.open("GET",'https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GDP-data.json',true);
-req.send();
-req.onload=function(){
-  json=JSON.parse(req.responseText);
-  const dataset = json.data
-  const title = json.source_name;
-  // console.log(dataset[0][0].substr(0,4))
-
-  document.getElementById('title').innerHTML=JSON.stringify(title);
 
 
-  // const width = 900;
-  // const height = 400;
-  const padding = 60;
+function Chart(data){
+
+  var svg = d3.select("#svg")
+  var padding={top: 50, right: 50, bottom: 50, left: 100};
+  
+  var ChartArea = {
+          "width": parseInt(svg.style("width")) - padding.left - padding.right,
+          "height": parseInt(svg.style("height")) - padding.top - padding.bottom,
+  };
+  
+  
+  
+  var yScale = d3.scaleLinear()
+  .domain([0, d3.max(data, function(d, i){return d[1]})])
+  .range([ChartArea.height,0])
+  
+  
+  var arr=[];
+  var t =data.map((d)=> {
+        y=  d[0].split("-");
+       var dataArr=y[0]
+       if(dataArr % 5 === 0){
+               return arr.push(dataArr)
+       }
+  })
+
+// console.log(arr)
+  // remove duplicate items
+
+  var parseTime =d3.timeParse("%m/%d/%y");
+  formatDate = d3.timeFormat("%Y");
+
+  var xScale=d3.scaleTime()
+  .domain(d3.extent(data, (d) => new Date(d[0])))
+  .range([0,ChartArea.width]);
+  
+  // Axis
+  // x-axis
+  var xAxis=svg.append("g")
+  .attr("id", "x-axis")
+  .attr("transform", "translate(0"+ padding.left + "," + (ChartArea.height + padding.top ) +")")
+  .call(d3.axisBottom(xScale));
+  
+  
+  
+  var yAxis=svg.append("g")
+  .attr("id", "y-axis")
+  .attr("transform", "translate(0"+ padding.left+ "," + padding.top+")")
+  .call( d3.axisLeft(yScale))
+  
+  var rectGrp= svg.append("g").attr(
+          "transform", "translate("+ padding.left + ","+ padding.top+")"
+  );
 
 
-  var svg = d3.select("svg"),
-  margin = 200,
-  width = svg.attr("width") - margin,
-  height = svg.attr("height") - margin;
- 
-var  OnlytheYears=(data) => {
- let a =data
- let y = a.split("-")
- 
-   let n = parseInt(y[0], 10)
-  //  console.log(n)
-   var num =n % 5 === 0;
-  if(num){
-   return n
+  svg.selectAll("div")
+  .data(data)
+  .enter()
+  .append("div")
+  .attr("x", (d) => d[0])
+  .attr("y", (d) =>  d[1])
+  .attr("width",(ChartArea.width/3))
+  .attr("height", () =>  ChartArea.height)
+  
+  
+  
+  rectGrp.selectAll("rect")
+  .data(data)
+    .enter()
+   .append("rect")
+   .attr("data-gdp", (d)=> d[1])
+   .attr("data-date", (d, i)=> d[0])
+   .attr("x", (d, i) => i*( ChartArea.width/data.length))
+   .attr("y", (d, i) =>  yScale(d[1]))
+   .attr("width",(ChartArea.width/data.length))
+   .attr("height", (d, i) =>  ChartArea.height - yScale(d[1]))
+  .attr("fill",(d,i)=>"yellow")
+  .attr("class", "bar")
+   .attr("id", "tooltip")
+   .append("title")
+   .text((d)=> d[0] +  " " + d[1])
+  
+  
+  
   }
-}
+  
 
-  var minData = d3.min(dataset,(d) =>  OnlytheYears(d[0]))
-  var maxData = d3.min(dataset,(d) =>  OnlytheYears(d[0]))
-
-        var xScale = d3.scaleBand().rangeRound([0, width]).paddingInner(0.05),
-  yScale = d3.scaleLinear().range([height , 0]);
-// console.log(xScale)
-var parseDate = d3.timeParse("%Y");
-var g = svg.append("g")
-.attr("id", "x-axis")
-         .attr("transform", "translate(" + 100 + "," + 100 + ")");
-
-         xScale.domain(dataset.map(function(d) { 
-         return  OnlytheYears(d[0])
-         }));
-
-         yScale.domain([0, d3.max(dataset, function(d) { return d[1]})]);
-
-         g.append("g")
-         .attr("id", "x-axis")
-         .attr("transform", "translate(0," + height + ")")
-         .call(d3.axisBottom(xScale));
-
-        g.append("g")
-        .attr("id", "y-axis")
-         .call(d3.axisLeft(yScale).tickFormat(function(d){
-             return  d;
-         }))
-   
-         svg.selectAll("rect")
-         .data(dataset)
-         .enter()
-         .append("rect")
-         .attr("class","bar")
-         .attr("data-date",(d)=> d[0])
-         .attr("data-gdp", (d)=> d[1])
-         .attr("x", (d, i) => i * 40)
-         .attr("y", (d, i) => {
-           // Add your code below this line
-          //  return height - 40 *
-           return height - 30 * d[1]
-           
-           
-           // Add your code above this line
-         })
-         .attr("width", 23)
-         .attr("height", (d, i) => d[1]);
-   
-    
-   
-//   document.getElementsByClassName('data')[0].innerHTML=JSON.stringify(dataset);
-};
+  
+  req=new XMLHttpRequest();
+  req.open("GET",'https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GDP-data.json',true);
+  req.send();
+  req.onload=function(){
+    json=JSON.parse(req.responseText);
+    var dataset = json.data;
+    var title = json.source_name;
+    document.getElementById('title').innerHTML=JSON.stringify(title);
+  
+  
+  Chart(dataset);
+  };
